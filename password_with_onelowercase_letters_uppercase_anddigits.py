5742b7c26e4aef0bc2a61c1179467cffd95f87c1
Somesh Debnath <91829500+Somesh-Debnath@users.noreply.github.com>
2021-12-06 15:20:39 +0530

    Update password_with_onelowercase_letters_uppercase_anddigits.py
    Added customised user input

--- password_with_onelowercase_letters_uppercase_anddigits.py
+++ password_with_onelowercase_letters_uppercase_anddigits.py
@@ -1,8 +1,9 @@
 import string
 import secrets
 alphabet = string.ascii_letters + string.digits
+n=int(input("Enter the size of the password: ")
 while True:
-    password = ''.join(secrets.choice(alphabet) for i in range(10))
+    password = ''.join(secrets.choice(alphabet) for i in range(n))
     if (any(c.islower() for c in password)
             and any(c.isupper() for c in password)
             and sum(c.isdigit() for c in password) >= 3):
